318ad0a81b00b2fe4e568e3480b2284d2ca9bcc0
Evan.Burbidge <burbidge@tcd.ie>
2020-04-01 16:25:49 +0100

    got working with DA spaces

--- src/io-methods/s3.js
+++ src/io-methods/s3.js
@@ -1,13 +1,16 @@
 const aws = require('aws-sdk');
-const { config } = require('../config');
+const { getConfig } = require('../config');
 const { parseFilePathForS3 } = require('../utils');
+
+const config = getConfig();
 
 if (config && config.region) {
   aws.config.update({ region: config.region });
 }
 
 const s3 = new aws.S3({
-  apiVersion: '2006-03-01'
+  apiVersion: '2006-03-01',
+  endpoint: config.s3Endpoint,
 });
 
 const readTranslations = filePath => new Promise((resolve, reject) => {
@@ -33,12 +36,7 @@ const writeTranslations = (filePath, data) => new Promise(async(resolve, reject)
     ContentType: 'application/json',
     CacheControl: 'public, must-revalidate, max-age=0',
     ACL: 'public-read',
-  }, function(err) {
-    if (err)
-      reject(err);
-    else
-      resolve();
-  });
+  }, err  => err ? reject(err) : resolve());
 });
 
 module.exports = {
